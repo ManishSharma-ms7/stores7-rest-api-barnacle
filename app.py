a781cab95975ed5112d7c37ca032dfa7e8520b95
vigilante <ms7.sharma1298@gmail.com>
2021-02-03 22:57:21 +0530

    db was not referenced

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'manish'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 jwt = JWT(app,authenticate,identity) # JWT creates a new endpoint
 
 api.add_resource(Store, '/store/<string:name>')
