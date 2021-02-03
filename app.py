d6859d311870bfa2c79da16dff3c646de5f1753e
vigilante <ms7.sharma1298@gmail.com>
2021-02-03 23:36:13 +0530

    added postgresql environment variable

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from flask_jwt import JWT
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'manish'
 api = Api(app)
